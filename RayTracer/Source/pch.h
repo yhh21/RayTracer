3547248cc1bec0423c267411ff7a60d1b9e12a7e
yhh21 <yinhuangfei@qq.com>
2019-02-18 17:41:55 +0800

    RayTracer

--- RayTracer/Source/pch.h
+++ RayTracer/Source/pch.h
@@ -5,14 +5,14 @@
 //#define DirectX12
 #ifdef DirectX12
 #include "d3dx12.h"
-#endif /// DirectX12
+#endif // DirectX12
 
 
-/// https://en.wikipedia.org/wiki/Netpbm_format
+// https://en.wikipedia.org/wiki/Netpbm_format
 #define PPM_FORMAT
 #ifdef PPM_FORMAT
 #define _CRT_SECURE_NO_WARNINGS
-#endif /// PPM_FORMAT
+#endif // PPM_FORMAT
 
 //#define NDEBUG
 #ifdef NDEBUG
@@ -25,28 +25,17 @@
 #include <cassert>
 #include "common/tool/DebugTools.h"
 
-/// glog formal
+// glog formal
 #define CHECK_EQ(val1, val2) assert(val1 == val2)
 #define CHECK_NE(val1, val2) assert(val1 != val2)
 #define CHECK_LE(val1, val2) assert(val1 <= val2)
 #define CHECK_LT(val1, val2) assert(val1 < val2)
 #define CHECK_GE(val1, val2) assert(val1 >= val2)
 #define CHECK_GT(val1, val2) assert(val1 > val2)
-#define CHECK(val1) assert(true == val1)
-#endif /// NDEBUG
+#define CHECK(val1) assert(false != val1)
+#endif // NDEBUG
 
 
 //#define FLOAT_AS_DOUBLE
-#ifdef FLOAT_AS_DOUBLE
-typedef double Float;
-#define FLOAT_0 0.0f
-#define FLOAT_1 1.0f
-#define FLOAT_2 2.0f
-#else
-typedef float Float;
-#define FLOAT_0 0.0F
-#define FLOAT_1 1.0F
-#define FLOAT_2 2.0F
-#endif
 
-using namespace std;
+#define ALLOCA(TYPE, COUNT) (TYPE *) alloca((COUNT) * sizeof(TYPE))
