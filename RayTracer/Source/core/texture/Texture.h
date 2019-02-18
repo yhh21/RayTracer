62657184550b44ebfe23a17ec1259dedf5d444a7
yhh21 <yinhuangfei@qq.com>
2019-02-18 17:41:30 +0800

    core texture

--- RayTracer/Source/core/texture/Texture.h
+++ RayTracer/Source/core/texture/Texture.h
@@ -1,9 +1,23 @@
 #pragma once
 
+#include "../../ForwardDeclaration.h"
+
 namespace core
 {
-    namespace texture
-    {
-        /// TODO
-    }
+namespace texture
+{
+
+
+template <typename T>
+class Texture
+{
+public:
+    virtual ~Texture()
+    {}
+
+    virtual T Evaluate(const interaction::SurfaceInteraction &si) const = 0;
+};
+
+
+}
 }
