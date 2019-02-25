5f6ca0181cf5211b297f31ba71bcd797bf96d8ed
yhh21 <yinhuangfei@qq.com>
2019-02-25 10:13:43 +0800

    common tool

--- RayTracer/Source/common/tool/MemoryArena.h
+++ RayTracer/Source/common/tool/MemoryArena.h
@@ -149,5 +149,113 @@ private:
 };
 
 
+template <typename T, int logBlockSize>
+class BlockedArray
+{
+public:
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // Construction
+    ////////////////////////////////////////////////////////////////////////////////
+
+    BlockedArray(int uRes, int vRes, const T *d = nullptr)
+        : uRes(uRes), vRes(vRes), uBlocks(RoundUp(uRes) >> logBlockSize)
+    {
+        int nAlloc = RoundUp(uRes) * RoundUp(vRes);
+        data = AllocAligned<T>(nAlloc);
+        for (int i = 0; i < nAlloc; ++i)
+        {
+            new (&data[i]) T();
+        }
+        if (d)
+        {
+            for (int v = 0; v < vRes; ++v)
+            {
+                for (int u = 0; u < uRes; ++u)
+                {
+                    (*this)(u, v) = d[v * uRes + u];
+                }
+            }
+        }
+    }
+
+    ~BlockedArray()
+    {
+        for (int i = 0; i < uRes * vRes; ++i)
+        {
+            data[i].~T();
+        }
+        FreeAligned(data);
+    }
+
+
+    constexpr int BlockSize() const
+    {
+        return 1 << logBlockSize;
+    }
+
+    int RoundUp(int x) const
+    {
+        return (x + BlockSize() - 1) & ~(BlockSize() - 1);
+    }
+
+
+    int uSize() const
+    {
+        return uRes;
+    }
+
+    int vSize() const
+    {
+        return vRes;
+    }
+
+    int Block(int a) const
+    {
+        return a >> logBlockSize;
+    }
+
+    int Offset(int a) const
+    {
+        return (a & (BlockSize() - 1));
+    }
+
+
+    T &operator()(int u, int v)
+    {
+        int bu = Block(u), bv = Block(v);
+        int ou = Offset(u), ov = Offset(v);
+        int offset = BlockSize() * BlockSize() * (uBlocks * bv + bu);
+        offset += BlockSize() * ov + ou;
+        return data[offset];
+    }
+
+    const T &operator()(int u, int v) const
+    {
+        int bu = Block(u), bv = Block(v);
+        int ou = Offset(u), ov = Offset(v);
+        int offset = BlockSize() * BlockSize() * (uBlocks * bv + bu);
+        offset += BlockSize() * ov + ou;
+        return data[offset];
+    }
+
+    void GetLinearArray(T *a) const
+    {
+        for (int v = 0; v < vRes; ++v)
+        {
+            for (int u = 0; u < uRes; ++u)
+            {
+                *a++ = (*this)(u, v);
+            }
+        }
+    }
+
+private:
+
+    T *data;
+    const int uRes, vRes, uBlocks;
+};
+
+
 }
 }
